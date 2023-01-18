5ffbe2b7e383ad86eecc345e2b2560b0f425a376
조민철 <jmc@bithumbsystems.com>
2023-01-18 13:50:23 +0900

    Fix: 예약 게시글 중복 기능 수정
    - 예약 게시글 중복 기능 수정
    - Swagger 수정

--- src/main/kotlin/com/bithumbsystems/cms/persistence/mongo/repository/CmsCommonRepository.kt
+++ src/main/kotlin/com/bithumbsystems/cms/persistence/mongo/repository/CmsCommonRepository.kt
@@ -2,6 +2,7 @@ package com.bithumbsystems.cms.persistence.mongo.repository
 
 import kotlinx.coroutines.flow.Flow
 import org.springframework.data.domain.Pageable
+import org.springframework.data.domain.Range
 import org.springframework.data.domain.Sort
 import org.springframework.data.mongodb.core.query.Criteria
 import org.springframework.data.repository.NoRepositoryBean
@@ -17,5 +18,5 @@ interface CmsCommonRepository<T> : CoroutineSortingRepository<T, String> {
 
     fun getFixItems(): Flow<T>
 
-    fun findByScheduleDateIsBetween(startTime: LocalDateTime, endTime: LocalDateTime): Flow<T>
+    fun findByScheduleDateIsBetween(dateTimeRange: Range<LocalDateTime>): Flow<T>
 }
